5a92e91cef53e582dd48fced4982ad845d5367a3
adrianzambrano94 <103285326+adrianzambrano94@users.noreply.github.com>
2022-09-03 11:10:50 -0500

    Creación de rama

--- js/progressbar.js
+++ js/progressbar.js
@@ -4,3 +4,23 @@ window.onscroll = function(){
   let progressHeight = (window.pageYOffset / totalHeight) * 100;
   progress.style.height = progressHeight + "%";
 }
+
+var targetOffset,
+    body = document.body,
+    button = document.getElementById('scrollButton'),
+    animateTime = 900;
+
+button.addEventListener('click', function (event) {
+  targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
+
+  body.style.transition = "margin-top " + animateTime + "ms ease-in-out";
+  body.style.marginTop = "-" + targetOffset + "px";
+
+  window.setTimeout(function () {
+    body.style.cssText = "";
+    window.scrollTo(0, targetOffset);
+  }, animateTime);
+
+  event.preventDefault();
+
+}, false);
